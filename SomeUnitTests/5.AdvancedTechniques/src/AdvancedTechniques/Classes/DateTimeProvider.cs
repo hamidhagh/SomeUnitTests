0a2d84ce2ef13bb46920d0bc063061058d470971
hamid haghverdi <hamidhaghverdi2002@gmail.com>
2024-09-12 10:18:35 -0700

    Advanced techniques added

---
+++ SomeUnitTests/5.AdvancedTechniques/src/AdvancedTechniques/Classes/DateTimeProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace AdvancedTechniques.Classes;
+
+public class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime DateTimeNow => DateTime.Now;
+}
+
+public interface IDateTimeProvider
+{
+    public DateTime DateTimeNow { get; }
+}
